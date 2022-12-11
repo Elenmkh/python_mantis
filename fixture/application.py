604d9069d01b7e6d4173c7718f6507c283b11f1e
Elena Makhnach <e.makhnach@mail.ru>
2022-12-11 21:22:52 +0300

    Добавлены методы для получения почты по протоколу POP3

--- fixture/application.py
+++ fixture/application.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
+from fixture.signup import SignupHelper
+from fixture.mail import MailHelper
 
 
 class Application:
@@ -20,8 +22,10 @@ class Application:
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
         self.james = JamesHelper(self)
+        self.mail = MailHelper(self)
+        self.signup = SignupHelper(self)
         self.config = config
-        self.base_url = config['web']["baseUrl"]
+        self.base_url = config['web']["baseURL"]
 
 
     def return_to_home_page(self):
